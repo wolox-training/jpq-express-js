ed8ac933bb47fd9518bc8fc788493755a7827561
Pablo Quiceno <pabloquiceno95@gmail.com>
2019-09-04 14:21:30 -0500

    CODER fixes

--- app/models/user.js
+++ app/models/user.js
@@ -3,10 +3,23 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     'User',
     {
-      name: DataTypes.STRING,
-      lastName: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
     },
     {}
   );
