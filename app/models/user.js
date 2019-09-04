a9856231b448fc64b35345aac07597b1dbb05b7f
Pablo Quiceno <pabloquiceno95@gmail.com>
2019-09-04 14:24:37 -0500

    CODER fixes

--- app/models/user.js
+++ app/models/user.js
@@ -3,6 +3,12 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     'User',
     {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
       name: {
         type: DataTypes.STRING,
         allowNull: false
