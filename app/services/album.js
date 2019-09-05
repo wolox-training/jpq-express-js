75b3d146311627c4d726bfa71e9d111b24e1c6cf
Pablo Quiceno <pabloquiceno95@gmail.com>
2019-09-05 09:18:47 -0500

    CODER fixes

--- app/services/album.js
+++ app/services/album.js
@@ -2,19 +2,15 @@ const requestPromise = require('request-promise');
 const { endpointJsonPlaceholder } = require('../../config').common.externalApi;
 const { externalError } = require('../errors');
 
-const getAlmbums = () => {
-  const promiseAlbums = requestPromise(`${endpointJsonPlaceholder}/albums`).catch(error => {
+const getAlmbums = () =>
+  requestPromise(`${endpointJsonPlaceholder}/albums`).catch(error => {
     throw externalError(error.message);
   });
-  return promiseAlbums;
-};
 
-const getPhotosByUser = userId => {
-  const promisePhotos = requestPromise(`${endpointJsonPlaceholder}/albums/${userId}/photos`).catch(error => {
+const getPhotosByUser = userId =>
+  requestPromise(`${endpointJsonPlaceholder}/albums/${userId}/photos`).catch(error => {
     throw externalError(error.message);
   });
-  return promisePhotos;
-};
 
 module.exports = {
   getAlmbums,
