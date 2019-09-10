9816bdd7f14d92598be9f6201424ab3696e79f2f
Pablo Quiceno <pabloquiceno95@gmail.com>
2019-09-10 15:56:33 -0500

    first deploy

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const app = require('./app');
-// const migrationsManager = require('./migrations');
+const migrationsManager = require('./migrations');
 const config = require('./config');
 const logger = require('./app/logger');
 
 const port = config.common.api.port || 8080;
 
 Promise.resolve()
-  // .then(() => migrationsManager.check())
+  .then(() => migrationsManager.check())
   .then(() => {
     app.listen(port);
 
     logger.info(`Listening on port: ${port}`);
   })
-  .catch(logger.error);
+  .catch(error => logger.error(error.message));
